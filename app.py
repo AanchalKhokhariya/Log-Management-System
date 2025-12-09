d165c12ce7194e5df531e2e0a3475a08b49273e3
Aanchal Khokhariya <aanchalkhokhariya07@gmail.com>
2025-12-09 19:02:42 +0530

    Adding Authorization

--- app.py
+++ app.py
@@ -18,12 +18,14 @@ class User(db.Model):
     name = db.Column(db.String(80), nullable=False, unique=True)
     gmail = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Integer, nullable=False) 
     log_entries = db.relationship('Log', backref='author', lazy=True)
 
 
 class Log(db.Model):
     __tablename__ = "logs"
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
     check_in = db.Column(db.Time, nullable=False)
     check_out = db.Column(db.Time, nullable=False)
     task = db.Column(db.Text, nullable=False)
@@ -36,17 +38,23 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("main.html", page="first_page")
+    return render_template("main.html", page="first_page", is_logged_in=("user_id" in session))
 
 
 @app.route("/home")
 def home1():
     if "user_id" in session:
         user = User.query.get(session["user_id"])
-        return render_template("demo.html", page="home", name=user.name)
+        return render_template("main.html", page="home", name=user.name, is_logged_in=True)
 
-    return render_template("main.html", page="Home")
+    return redirect(url_for("home"))
 
+@app.context_processor
+def inject_user():
+    return {
+        "name": session.get("name"),
+        "is_logged_in": "user_id" in session
+    }
 
 
 @app.route("/register")
@@ -56,24 +64,42 @@ def show_register():
 
 @app.route("/register", methods=["POST"])
 def register():
+    selected_role = request.form.get('role')
     name = request.form["name"]
     gmail = request.form["gmail"]
     password = request.form["password"]
     confirm = request.form["confirm"]
 
     if password != confirm:
-        return render_template("main.html", page="register",error="Passwords do not match",is_logged_in="user_id" in session)
+        return render_template("main.html", page="register", error="Passwords do not match", is_logged_in="user_id" in session)
+
+    existing_user = User.query.filter((User.name == name) | (User.gmail == gmail)).first()
+
+    if existing_user:
+        return render_template("main.html", page="register", error="User already exists! Try another name or gmail.")
 
     try:
         hashed = generate_password_hash(password)
-        new_user = User(name=name, gmail=gmail, password=hashed)
+
+        if selected_role == "admin":
+            role_value = 1
+        else:
+            role_value = 0
+
+        new_user = User(
+            name=name,
+            gmail=gmail,
+            password=hashed,
+            role=role_value
+        )
+        
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for("login"))
 
     except:
         db.session.rollback()
-        return render_template("main.html", page="register", error="Registration Failed",is_logged_in="user_id" in session)
+        return render_template("main.html", page="register", error="Registration Failed", is_logged_in="user_id" in session)
 
 
 @app.route("/login")
@@ -92,16 +118,21 @@ def login():
     if user and check_password_hash(user.password, password):
         session["user_id"] = user.id
         session["name"] = user.name
-        return render_template("main.html", page="logs", name=name, is_logged_in=True)
-    else:
-        return render_template("main.html", page="login", error="Invalid Login", is_logged_in=False)
+        
+        if user.role == 1:
+            return redirect(url_for("user_logs"))
+        else:
+            return render_template("main.html", page="logs", name=name, is_logged_in=True)
+
+    return render_template("main.html", page="login", error="Invalid Login", is_logged_in=False)
 
 
 @app.route("/logs", methods=["POST"])
 def logs():
     if "user_id" not in session:
         return redirect(url_for("login"))
-
+    
+    date = request.form["date"]
     check_in = request.form["check_in"]
     check_out = request.form["check_out"]
     task = request.form["task"]
@@ -115,6 +146,7 @@ def logs():
             return "Check-out time must be after check-in time"
 
         new_log = Log(
+            date = date,
             check_in=t1.time(),
             check_out=t2.time(),
             task=task,
@@ -129,13 +161,48 @@ def logs():
 
     except:
         db.session.rollback()
-        return render_template("main.html", page="logs",error="Failed to add log", is_logged_in=True)
+        return render_template("main.html", page="logs", error="Failed to add log", is_logged_in=True)
+    
+    
+@app.route("/user_logs", methods=["GET", "POST"])
+def user_logs():
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+
+    current_user = User.query.get(session["user_id"])
+    if current_user.role != 1:
+        return "Access Denied"
+
+    if request.method == "GET":
+        user_names = User.query.all()
+        return render_template("main.html", page="user_logs", user_names=user_names, is_logged_in=True)
+
+    if request.method == "POST":
+        uid = request.form.get("user_id")
+        logs = Log.query.filter_by(user_id=uid).all()
+        user = User.query.get(uid)
+        return render_template("main.html", page="view_logs", logs=logs, name=user.name, is_logged_in=True)
+
+
+@app.route("/view_logs")
+def view_user_logs(user_id):
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+
+    current_user = User.query.get(session["user_id"])
+    if current_user.role != 1:   
+        return "Access Denied"
+
+    logs = Log.query.filter_by(user_id=user_id).all()
+    user = User.query.get(user_id)
+
+    return render_template("main.html", page="view_logs", logs=logs, name=user.name, is_logged_in=True)
 
 
 @app.route("/list")
 def list_log():
     if "user_id" not in session:
-        return redirect(url_for("show_login"))
+        return redirect(url_for("login"))
 
     data = Log.query.filter_by(user_id=session["user_id"]).all()
     return render_template("main.html", page="list", data=data, is_logged_in=True)
