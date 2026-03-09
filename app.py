e2141aabb93b20762e61da546adacfb37bdd622a
Aanchal Khokhariya <aanchalkhokhariya07@gmail.com>
2026-03-09 10:22:12 +0530

    change in env file

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = "supersecretkey"
+app.secret_key = os.getenv("SECRET_KEY")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:2807@localhost/user_log"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
